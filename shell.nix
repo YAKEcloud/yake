66467ca0389148c41d28454f56b1ffe8af62f710
Daniel Poelzleithner <git@poelzi.org>
2021-05-11 12:14:54 +0200

    add cert-manager cli, set upload env

--- shell.nix
+++ shell.nix
@@ -1,4 +1,6 @@
-{ sources ? import ./nix/sources.nix { }}:
+{ sources ? import ./nix/sources.nix { },
+  # should be false on derived configs
+  uploadDevConfig ? true}:
 let 
   pkgs = import sources.nixpkgs {};
 in
@@ -31,6 +33,54 @@ let
     };
   };
 
+  charts-syncer = buildGoModule rec {
+    pname = "charts-syncer";
+    version = "0.9.0";
+
+    src = fetchFromGitHub {
+      owner = "bitnami-labs";
+      repo = "charts-syncer";
+      rev = "v${version}";
+      sha256 = "0cj29ppsr4xnncxrgq5xg9v0j8sgi59mx24vlnxr8rba80siqhh2";
+    };
+
+    vendorSha256 = "0hky6jz3kpslkad1sn9gnyspw1crl4fn20m85vh5hcqwdvyiflw7";
+
+    doCheck = false;
+
+    meta = with lib; {
+      inherit (src.meta) homepage;
+      description =
+        "Tool for synchronizing Helm Chart repositories.";
+      license = licenses.asl20;
+    };
+  };
+
+  cert-manager-cli = buildGoModule rec {
+    pname = "charts-syncer";
+    version = "1.3.1";
+
+    src = fetchFromGitHub {
+      owner = "jetstack";
+      repo = "cert-manager";
+      rev = "v${version}";
+      sha256 = "0gcshqhzmc6dyw448npir9i7b2dqlpzlaaffq3smi569clz7dmns";
+    };
+
+    vendorSha256 = "1igmmwgn4mbbhrnc02f3pjdpx10c06a043x8qgqc950ra3cjcp7g";
+
+    doCheck = false;
+
+    subPackages = [ "cmd/ctl" ];
+
+    meta = with lib; {
+      inherit (src.meta) homepage;
+      description =
+        "Tool for synchronizing Helm Chart repositories.";
+      license = licenses.asl20;
+    };
+  };
+
   # myTerraform = pkgs.terraform_0_13 {
   #       plugins = [terraform-kubectl];
   #     };
@@ -40,6 +90,8 @@ in pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     coreutils
     clusterctl
+    charts-syncer
+    cert-manager-cli
     docker
     fluxcd
     git
@@ -65,5 +117,6 @@ in pkgs.mkShell {
 
     export KE23_ROOT=`pwd`
     export PATH=$PATH:$KE23_ROOT/hack
+    export KE23_UPLOAD_DEV_CONFIG=${(if uploadDevConfig then "1" else "0")}
   '';
 }
