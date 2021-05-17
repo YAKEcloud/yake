2010044d38007dafa92e1ebfdffd23682b0ff885
Lothar Gesslein <gesslein@23technologies.cloud>
2021-05-17 20:06:32 +0200

    use hostname from ingress for curl test

--- hack/ci/test-kind.sh
+++ hack/ci/test-kind.sh
@@ -3,6 +3,8 @@
 set +e
 kubectl get -n flux-system secrets dev-ca --output='go-template={{index .data "ca.crt"}}' | base64 -d  > /tmp/kind-ca
 
+dashboard_url=$(kubectl get ingress gardener-dashboard-ingress -n garden -o json | jq -r .spec.rules[0].host)
+
 kubectl get -n garden secrets/garden-kubeconfig-for-admin --template={{.data.kubeconfig}} | base64 -d > /tmp/garden-kubeconfig
 export KUBECONFIG=/tmp/garden-kubeconfig
 
@@ -18,4 +20,4 @@ kubectl explain seed > /dev/null
 echo "--------------------"
 echo "check webui"
 
-curl --cacert /tmp/kind-ca https://dashboard.dev.local.a38.link/login -o /dev/null
+curl --cacert /tmp/kind-ca https://${dashboard_url}/login -o /dev/null
