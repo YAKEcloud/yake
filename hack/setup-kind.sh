3a802738859c2122cc11de5cc28dbf14b70580ac
Lothar Gesslein <gesslein@23technologies.cloud>
2021-05-17 16:24:05 +0200

    wait for minio deployment

--- hack/setup-kind.sh
+++ hack/setup-kind.sh
@@ -15,6 +15,7 @@ hack/metallb/setup-lb.sh
 
 echo "Waiting for end point..."
 minio_ip="$(./hack/wait-for-service.sh minio default)"
+kubectl wait --for=condition=available --timeout=1m  deployment minio -n default
 echo "Minio ready at ${minio_ip}"
 mc --config-dir hack/mc-config config host add kind http://"${minio_ip}":9000 minio minio123
 
