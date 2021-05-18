f2f3414e9654244e1a56983a3c7213788c7adf38
Lothar Gesslein <gesslein@23technologies.cloud>
2021-05-18 07:57:35 +0200

    use lowercase branch name

--- hack/ci/github-setup-env.sh
+++ hack/ci/github-setup-env.sh
@@ -5,8 +5,8 @@
 # GITHUB_BASE_REF is set only when triggered by a pull request
 # it refers to the branch where the PR should be merged to.
 if [[ -z "${GITHUB_BASE_REF}" ]]; then
-  export BRANCH_NAME="$(echo "${GITHUB_REF}" | sed 's/refs\/heads\///' | tr '[:lower:]-/' '[:upper:]__')"
+  export BRANCH_NAME="$(echo "${GITHUB_REF}" | sed 's/refs\/heads\///' | tr '[:upper:]-/' '[:lower:]__')"
 else
-  export BRANCH_NAME="$(echo "${GITHUB_BASE_REF}" | tr '[:lower:]-/' '[:upper:]__')"
+  export BRANCH_NAME="$(echo "${GITHUB_BASE_REF}" | tr '[:upper:]-/' '[:lower:]__')"
 fi
 echo "BRANCH_NAME=${BRANCH_NAME}"
