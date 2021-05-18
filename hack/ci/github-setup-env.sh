a32affb12d9ec057a68ca1c3cb12eb611f7595d8
Lothar Gesslein <gesslein@23technologies.cloud>
2021-05-18 08:02:14 +0200

    use dash in branch name

--- hack/ci/github-setup-env.sh
+++ hack/ci/github-setup-env.sh
@@ -5,8 +5,8 @@
 # GITHUB_BASE_REF is set only when triggered by a pull request
 # it refers to the branch where the PR should be merged to.
 if [[ -z "${GITHUB_BASE_REF}" ]]; then
-  export BRANCH_NAME="$(echo "${GITHUB_REF}" | sed 's/refs\/heads\///' | tr '[:upper:]-/' '[:lower:]__')"
+  export BRANCH_NAME="$(echo "${GITHUB_REF}" | sed 's/refs\/heads\///' | tr '[:upper:]_/' '[:lower:]--')"
 else
-  export BRANCH_NAME="$(echo "${GITHUB_BASE_REF}" | tr '[:upper:]-/' '[:lower:]__')"
+  export BRANCH_NAME="$(echo "${GITHUB_BASE_REF}" | tr '[:upper:]_/' '[:lower:]--')"
 fi
 echo "BRANCH_NAME=${BRANCH_NAME}"
