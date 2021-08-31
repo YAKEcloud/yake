0d8aee5b027ac66e1d2db59bebe319af0ab1eeaa
Malte Münch <muench@23technologies.cloud>
2021-08-31 18:02:47 +0200

    shoot delete and crete scripts
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/delete-shoot.sh
+++ hack/delete-shoot.sh
@@ -2,6 +2,19 @@
 export SHOOT=$(yq eval '.clusters[0].name' hack/shoot-kubeconfig.yaml|sed 's/shoot--23t-test--//')
 export KUBECONFIG=.github/gardener-kubeconfig.yaml
 # Annotate Shoot for deletion
-kubectl annotate shoot -n garden-23t-test $SHOOT confirmation.gardener.cloud/deletion=true
+kubectl annotate shoot -n garden-23t-test $SHOOT confirmation.gardener.cloud/deletion=true > /dev/null 2>&1
 # Delete shoot
-kubectl delete shoot -n garden-23t-test $SHOOT
+kubectl delete shoot -n garden-23t-test $SHOOT --wait=false > /dev/null || (echo -e "Shoot deletion unsuccessful❌\nexiting..."; exit 1)
+echo -n "Deleting shoot..."
+OLD_PERCENTAGE=0
+while kubectl get shoot $SHOOT -n garden-23t-test > /dev/null 2>&1
+do
+    PERCENTAGE=$(kubectl get shoot -n garden-23t-test $SHOOT -o jsonpath="{.status.lastOperation.progress}" 2> /dev/null)
+    if [ $OLD_PERCENTAGE -lt $PERCENTAGE ]
+    then
+        echo -n -e "\rDeleting shoot: $PERCENTAGE%"
+        OLD_PERCENTAGE=$PERCENTAGE
+    fi
+	sleep 1
+done
+echo  -e "\rshoot deleted 🗑       "
--- hack/setup-shoot.sh
+++ hack/setup-shoot.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bash
 RAND=$(openssl rand -hex 2)
+export MINIO_RAND=$(openssl rand -hex 20)
 export SHOOT="23ke-run-$RAND"
+export MINIO_HOSTNAME="minio.$SHOOT.23t-test-okeanos.dev"
+
 export KUBECONFIG=.github/gardener-kubeconfig.yaml
 
 # Alter shoot template
 yq eval '.metadata.name = env(SHOOT)' -i hack/shoot-template.yaml
 
 # Create Shoot
-kubectl apply -f hack/shoot-template.yaml || ( echo "kubectl apply unsuccessful, exiting..." && exit 1 )
+kubectl apply -f hack/shoot-template.yaml > /dev/null || ( echo "Shoot creation unsuccessful❌\nexiting..." && exit 1 )
 
 # Restore shoot template
-git checkout hack/shoot-template.yaml
+git checkout -q hack/shoot-template.yaml
 
 # Wait for shoot to become available
-while [ ! "$(kubectl get shoot $SHOOT -n garden-23t-test -o jsonpath="{.status.lastOperation.state}")" == "Succeeded" ]
+echo -n "Creating shoot: 0%"
 OLD_PERCENTAGE=0
+while [ ! "$(kubectl get shoot $SHOOT -n garden-23t-test -o jsonpath="{.status.lastOperation.state}")" == "Succeeded" ]
 do
-    PERCENTAGE=$(kubectl get shoot -n garden-23t-test 23ke-run-f0d2 -o jsonpath="{.status.lastOperation.progress}")
+    PERCENTAGE=$(kubectl get shoot -n garden-23t-test $SHOOT -o jsonpath="{.status.lastOperation.progress}")
     if [ $OLD_PERCENTAGE -lt $PERCENTAGE ]
     then
-        echo "waiting for Shoot... $PERCENTAGE %"
+        echo -n -e "\rCreating shoot: $PERCENTAGE%"
         OLD_PERCENTAGE=$PERCENTAGE
     fi
 	sleep 1
 done
+echo  -e "\rshoot ready ✅               "
 
 # Get shoot kubeconfig
 kubectl get secret -n garden-23t-test $SHOOT.kubeconfig -o go-template='{{.data.kubeconfig|base64decode}}' > hack/shoot-kubeconfig.yaml
@@ -31,19 +36,18 @@ kubectl get secret -n garden-23t-test $SHOOT.kubeconfig -o go-template='{{.data.
 export KUBECONFIG=hack/shoot-kubeconfig.yaml
 
 
-export MINIO_RAND=$(openssl rand -hex 20)
-export MINIO_HOSTNAME="minio.$SHOOT.23t-test-okeanos.dev"
-
 # Alter minio template
 yq eval 'select(documentIndex == 1) .spec.template.spec.containers[0].env[1].value = env(MINIO_RAND)' -i hack/minio.yaml
 yq eval 'select(documentIndex == 2) .metadata.annotations."dns.gardener.cloud/class" = "garden"' -i hack/minio.yaml
 yq eval 'select(documentIndex == 2) .metadata.annotations."dns.gardener.cloud/dnsnames" = env(MINIO_HOSTNAME)' -i hack/minio.yaml
 
 # Install minio
-kubectl apply -f hack/minio.yaml
+kubectl apply -f hack/minio.yaml > /dev/null || ( echo "Minio deployment unsuccessful❌\nexiting..." && exit 1 )
 
 # Restore minio template
-git checkout hack/minio.yaml
+git checkout -q hack/minio.yaml
 
+echo -n "Deploying minio..."
 # Wait for minio deployment to become ready
-kubectl wait --for=condition=available --timeout=1m  deployment minio -n default
+kubectl wait --for=condition=available --timeout=1m  deployment minio -n default > /dev/null || ( echo "Minio deployment unsuccessful❌\nexiting..." && exit 1 )
+echo  -e "\rMinio ready ✅       "
