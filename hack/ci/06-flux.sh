ee60251379d46ddc56567f976d0bc6913d9d48de
Malte Münch <muench@23technologies.cloud>
2021-09-14 11:50:25 +0200

    switch to upstream flux
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/ci/06-flux.sh
+++ hack/ci/06-flux.sh
@@ -1,7 +1,9 @@
 #/usr/bin/env bash
 
 source hack/handy.sh
-
+wget https://github.com/fluxcd/flux2/releases/download/v0.17.1/flux_0.17.1_linux_amd64.tar.gz
+tar xfvz flux_0.17.1_linux_amd64.tar.gz
+cp flux /tmp/flux
 # Install flux
 echo -n "Installing Flux"
 flux install > /dev/null 2>&1 || { echo "Error while installing Flux ❌" ; exit 1; }
@@ -34,13 +36,11 @@ then
 	kubectl create secret generic -n flux-system minio-local --from-literal=accesskey=minio --from-literal=secretkey=$MINIO_PW > /dev/null 2>&1 || { echo "Error while Creating secret. ❌" ; exit 1; }
 fi
 echo -n "."
-flux --version
-flux create source bucket $BUCKET --endpoint=$MINIO_HOSTNAME --bucket-name=$BUCKET --secret-ref=minio-local  --export
-flux create source bucket $BUCKET --endpoint=$MINIO_HOSTNAME --bucket-name=$BUCKET --secret-ref=minio-local  --debug || { echo "Error while creating flux 23ke bucket source ❌" ; exit 1; }
+/tmp/flux create source bucket $BUCKET --endpoint=$MINIO_HOSTNAME --bucket-name=$BUCKET --secret-ref=minio-local || { echo "Error while creating flux 23ke bucket source ❌" ; exit 1; }
 echo -n "."
 flux create kustomization $BUCKET --source=Bucket/$BUCKET > /dev/null 2>&1 || { echo "Error while creating flux 23ke kustomization ❌" ; exit 1; }
 echo -n "."
-flux create source bucket 23ke-config --endpoint=$MINIO_HOSTNAME --bucket-name=config --secret-ref=minio-local > /dev/null 2>&1 || { echo "Error while creating flux 23ke-config bucket source ❌" ; exit 1; }
+/tmp/flux create source bucket 23ke-config --endpoint=$MINIO_HOSTNAME --bucket-name=config --secret-ref=minio-local > /dev/null 2>&1 || { echo "Error while creating flux 23ke-config bucket source ❌" ; exit 1; }
 echo -n "."
 if ! kubectl -n flux-system get ks config > /dev/null 2>&1
 then
