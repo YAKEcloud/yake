8557d0d704df72ed8b584d194d481f7d12cb439d
Malte Münch <muench@23technologies.cloud>
2021-09-23 09:28:21 +0200

    extend tests
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/ci/07-tests.sh
+++ hack/ci/07-tests.sh
@@ -3,6 +3,10 @@
 source hack/handy.sh
 
 echo -n "Running tests"
+kubectl wait kustomization 23ke-base -n flux-system --for=condition=ready --timeout=10m > /dev/null 2>&1 || { echo -e "\rError while waiting for flux-kustomization 23ke-base ❌"; exit 1; }
+echo -n "."
+kubectl wait kustomization 23ke-env -n flux-system --for=condition=ready --timeout=10m > /dev/null 2>&1 || { echo -e "\rError while waiting for flux-kustomization 23ke-env ❌"; exit 1; }
+echo -n "."
 kubectl wait kustomization -A --all --for=condition=ready --timeout=10m > /dev/null 2>&1 || { echo -e "\rError while waiting for flux-kustomizations ❌"; exit 1; }
 echo -n "."
 kubectl wait helmrelease -A --all --for=condition=ready --timeout=20m > /dev/null 2>&1 || { echo -e "\rError while waiting for helmreleases ❌"; exit 1; }
