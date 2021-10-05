cc71ed05e33677133a9bc9a7d34e7deb68f241aa
Malte Münch <muench@23technologies.cloud>
2021-10-05 15:05:39 +0200

    Template hostname of gardener-apisever
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/ci/05-config-bucket.sh
+++ hack/ci/05-config-bucket.sh
@@ -8,10 +8,19 @@ echo  -n -e "\rConfig Bucket creating"
 echo -n "."
 mc cp --recursive hack/dev-env $MC_ALIAS/$CONFIG_BUCKET > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while uploading Config to Bucket ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 echo -n "."
+# Templating gardener-values.yaml
 yq eval '.stringData."values.yaml"' hack/dev-env/config/gardener-values.yaml.tmpl > /tmp/values.yaml
 yq eval '.global.internalDomain.domain = "internal." + env(SHOOT) + ".23t-test.okeanos.dev"' -i /tmp/values.yaml
 yq eval '.stringData."values.yaml" = "'"$(< /tmp/values.yaml)"'"' hack/dev-env/config/gardener-values.yaml.tmpl > hack/dev-env/config/gardener-values.yaml
+
+# Templating 23ke-kube-apiserver-values.yaml
+yq eval '.data."values.yaml"' hack/dev-env/config/kube-apiserver-values.yaml.tmpl > /tmp/values.yaml
+yq eval '.ingress.annotations."dns.gardener.cloud/dnsnames" = "api.dev.23ke-run-" + env(SHOOT) + ".23t-test.okeanos.dev' -i /tmp/values.yaml
+export VALUES=$(cat /tmp/values.yaml)
+yq eval '.data."values.yaml" = strenv(VALUES)' hack/dev-env/config/kube-apiserver-values.yaml.tmpl > hack/dev-env/config/kube-apiserver-values.yaml
+
 mc cp hack/dev-env/config/gardener-values.yaml $MC_ALIAS/$CONFIG_BUCKET/dev-env/config/ > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while uploading gardener-values.yaml to Bucket ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 echo -n "."
 rm hack/dev-env/config/gardener-values.yaml
+rm hack/dev-env/config/kube-apiserver-values.yaml
 echo -e "\rConfig Bucket ready  ✅       "
