e8c97d8d6024877ff2686df7383348c7b010bca5
Malte Münch <muench@23technologies.cloud>
2021-11-29 15:10:20 +0100

    use native runner
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

---
+++ hack/ci/00-packages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# kubectl
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo mv kubectl /usr/local/bin/
+sudo chmod +x /usr/local/bin/kubectl
+
+# flux
+wget https://github.com/fluxcd/flux2/releases/download/v0.17.2/flux_0.17.2_linux_amd64.tar.gz
+tar xfvz flux_0.17.2_linux_amd64.tar.gz
+sudo mv flux /usr/local/bin/
+sudo chmod +x /usr/local/bin/flux
+
+# yq
+wget https://github.com/mikefarah/yq/releases/download/v4.15.1/yq_linux_amd64
+sudo mv yq_linux_amd64 /usr/local/bin/yq
+sudo chmod +x /usr/local/bin/yq
+
+# helm
+wget https://get.helm.sh/helm-v3.7.1-linux-amd64.tar.gz
+tar xfvz helm-v3.7.1-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+sudo chmod +x /usr/local/bin/helm
+
