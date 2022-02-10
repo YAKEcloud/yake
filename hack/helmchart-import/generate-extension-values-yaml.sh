7ddc49b3a809e88e80bc2952a681771e9cab9113
Jens Schneider <schneider@23technologies.cloud>
2022-02-10 16:50:56 +0100

    Add script for generation of values.yaml for helmchartextsion import
    Moreover, modify the import script such that it contains the templating.
    
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

---
+++ hack/helmchart-import/generate-extension-values-yaml.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+if [[ $1 == "" ]]
+then
+   echo "usage: ./generate-values-yaml.sh <extension_name> [tag]"
+   echo	"example: ./generate-values-yaml.sh gardener/external-dns-management"
+   echo	"example: ./generate-values-yaml.sh gardener/external-dns-management v1.1.1"
+   exit
+fi
+DEP_NAME=$1
+
+# in case jq and yq are not installed
+JQ=/tmp/jq
+if ! [ -x $JQ ]
+then
+    wget -q https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O /tmp/jq
+    chmod +x $JQ
+fi
+
+YQ=/tmp/yq
+if ! [ -x $YQ ]
+then
+   wget -q https://github.com/mikefarah/yq/releases/download/v4.19.1/yq_linux_amd64 -O /tmp/yq
+   chmod +x $YQ
+fi
+
+# define the latest release url for the current dependency
+if [[ $2 == "" ]]
+then
+    URL="https://api.github.com/repos/$DEP_NAME/releases/latest"
+    TAG=$(wget -q $URL -O - | $JQ -r '.tag_name' )
+else
+    TAG=$2
+fi
+
+EXT_NAME=$(echo $DEP_NAME | sed 's/\//-/')
+echo $EXT_NAME:
+echo "  enable: false"
+echo "  values:"
+echo "    image:"
+echo "      # renovate: depName=$DEP_NAME"
+echo "      tag: $TAG"
+echo "  resources:"
+
+if ! wget -q https://raw.githubusercontent.com/$DEP_NAME/$TAG/example/controller-registration.yaml -O /tmp/controller-registration.yaml
+then
+     wget -q https://raw.githubusercontent.com/$DEP_NAME/$TAG/examples/controller-registration.yaml -O /tmp/controller-registration.yaml
+fi
+
+$YQ eval 'select(documentIndex == 1).spec.resources' /tmp/controller-registration.yaml | sed 's/^/    /'
--- hack/helmchart-import/import-extension-charts.sh
+++ hack/helmchart-import/import-extension-charts.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+YQ=/tmp/yq
+if ! [ -x $YQ ]
+then
+   wget -q https://github.com/mikefarah/yq/releases/download/v4.19.1/yq_linux_amd64 -O /tmp/yq
+   chmod +x $YQ
+fi
+
 # define file providing the data
 # the script is expected to be executed in the repository root
 FILE=helmcharts/extensions/values.yaml
@@ -15,14 +22,33 @@ do
     # extract extension name and tag from the tuple
     EXT=$(echo $extension | cut -d ' ' -f 3 | sed 's/depName=//')
     TAG=$(echo $extension | cut -d ' ' -f 5)
-
-    # construct the url of the file to be fetched
-    FILE_URL=https://raw.githubusercontent.com/$EXT/$TAG/example/controller-registration.yaml
+    EXT_NAME=$(echo $EXT | sed 's/\//-/')
 
     # construct the target filename
-    FILE_NAME=$TARGET_DIR/$(echo $EXT | sed 's/\//-/').yaml
+    FILE_NAME=$TARGET_DIR/$EXT_NAME.yaml
 
-    # fetch the file
-    wget $FILE_URL -O $FILE_NAME > /tmp/stdout 2> /tmp/stderr || { echo -e "\r Error during fetching extension $EXT ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr;}
+    # fetch the upstream controller-registration file
+    wget -q https://raw.githubusercontent.com/$EXT/$TAG/example/controller-registration.yaml -O /tmp/controller-registration.yaml || \
+	wget -q https://raw.githubusercontent.com/$EXT/$TAG/examples/controller-registration.yaml -O /tmp/controller-registration.yaml || \
+	wget -q https://github.com/$EXT/releases/download/$TAG/controller-registration.yaml -O /tmp/controller-registration.yaml
 
+    $YQ eval 'select(documentIndex == 0)' /tmp/controller-registration.yaml > /tmp/deployment.yaml
+    $YQ eval 'select(documentIndex == 1)' /tmp/controller-registration.yaml > /tmp/registration.yaml
+
+    # replace values in the deploymnent
+    $YQ eval '.providerConfig.values="replaceMe"' -i /tmp/deployment.yaml
+    sed -i 's/ replaceMe//' /tmp/deployment.yaml
+    echo "{{- toYaml .Values.$EXT_NAME.values | nindent 4 }}" >> /tmp/deployment.yaml
+
+    # replace resources in the controller registration
+    $YQ eval '.spec.resources="replaceMe"' -i /tmp/registration.yaml
+    sed -i 's/ replaceMe//' /tmp/registration.yaml
+    echo "{{- toYaml .Values.$EXT_NAME.resources | nindent 4 }}" >> /tmp/registration.yaml
+
+    # merge the two separate files and
+    echo "{{- if .Values.$EXT_NAME.enabled }}" > $FILE_NAME
+    cat /tmp/deployment.yaml >> $FILE_NAME
+    echo "---" >> $FILE_NAME
+    cat /tmp/registration.yaml >> $FILE_NAME
+    echo "{{- end }}" >> $FILE_NAME
 done
