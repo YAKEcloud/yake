ab4b4329e0092685ffd3da2c30bb0c988da53657
Fynn Späker <spaeker@23technologies.cloud>
2022-03-02 15:27:12 +0100

    refine testing (#74)
    * Add gardenlet configuration to main script
    
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>
    
    * Add demo application to the testing process
    
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>
    
    * Use containerd instead of docker
    
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>
    
    * Refine Output and use right kubeconfig
    
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>
    
    * Change wait trigger
    
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>
    
    * use namespace for the demo app
    
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>
    
    * Fix hcloud token inside the github action
    
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>

---
+++ hack/ci/09-demo-shoot.sh
@@ -0,0 +1,33 @@
+#/usr/bin/env bash
+
+source hack/ci/handy.sh
+
+echo -e -n "\rDeploying sample Shoot"
+
+kubectl apply -f hack/ci/misc/shoot-microservice.yaml --context garden > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while applying testing shoot ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+echo -n "."
+
+echo -n -e "\rCreating shoot: 0%        "
+OLD_PERCENTAGE=0
+while [ ! "$(kubectl get shoot microservice -n garden-testing -o jsonpath="{.status.lastOperation.state}" --context garden)" == "Succeeded" ]
+do
+    sleep 5
+    PERCENTAGE=$(kubectl get shoot -n garden-testing microservice -o jsonpath="{.status.lastOperation.progress}" --context garden)
+    if [ $OLD_PERCENTAGE -le $PERCENTAGE ]
+    then
+        echo -n -e "\rCreating shoot: $PERCENTAGE%         "
+        OLD_PERCENTAGE=$PERCENTAGE
+    fi
+	sleep 1
+done
+
+kubectl get secret -n garden-testing microservice.kubeconfig -o go-template='{{.data.kubeconfig|base64decode}}' --context garden > hack/ci/secrets/shoot-microservice-kubeconfig.yaml 
+echo -n -e "\rDeploying sample shoot: .           "
+
+kubectl apply -n default -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/main/release/kubernetes-manifests.yaml --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml > /tmp/stdout 2> /tmp/stderr || { echo -e "\rUnsuccessful microservice deployment            ❌               "; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+echo -e -n "\rWaiting for microservice demo:         "
+sleep 5
+kubectl wait --for=condition=available --timeout=600s -n default deployment/loadgenerator --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml > /tmp/stdout 2> /tmp/stderr || { echo -e "\rUnsuccessful microservice deployment            ❌               "; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+
+
+echo -e "\rDemo-Deployment ready✅                "
