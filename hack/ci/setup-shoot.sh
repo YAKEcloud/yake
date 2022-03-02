c2ef678659ad4fcd11d4c6afe12b212558d99b07
Fynn Späker <spaeker@23technologies.cloud>
2022-03-02 11:56:09 +0100

    Add gardenlet configuration to main script
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>

--- hack/ci/setup-shoot.sh
+++ hack/ci/setup-shoot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 clear
-export PROGRESS_WIDTH=80
-export PROGRESS_NUMBER_OF_STEPS=8
+export PROGRESS_WIDTH=90
+export PROGRESS_NUMBER_OF_STEPS=9
 
 # Setup Environment
 bash hack/ci/00-environment.sh || exit 1
@@ -35,4 +35,8 @@ progress_step
 
 # Install 23KE
 bash hack/ci/07-23ke.sh || exit 1
+progress_step
+
+# Add hcloud secret
+bash hack/ci/08-gardenlet.sh || exit 1
 progress_step
