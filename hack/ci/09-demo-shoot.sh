05b81f65d9c9a5791b836791576b1faf7d469f89
Fynn Späker <spaeker@23technologies.cloud>
2022-03-02 12:56:24 +0100

    Change wait trigger
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>

--- hack/ci/09-demo-shoot.sh
+++ hack/ci/09-demo-shoot.sh
@@ -27,7 +27,7 @@ echo -n -e "\rDeploying sample shoot: .           "
 kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/main/release/kubernetes-manifests.yaml --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml > /tmp/stdout 2> /tmp/stderr || { echo -e "\rUnsuccessful microservice deployment            ❌               "; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 echo -e -n "\rWaiting for microservice demo:         "
 sleep 5
-kubectl wait --for=condition=ready --timeout=600s deployment/loadgenerator --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml 2> /tmp/stderr || { echo -e "\rUnsuccessful microservice deployment            ❌               "; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+kubectl wait --for=condition=available --timeout=600s deployment/loadgenerator --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml > /tmp/stdout 2> /tmp/stderr || { echo -e "\rUnsuccessful microservice deployment            ❌               "; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 
 
 echo -e "\rDemo-Deployment ready✅                "
