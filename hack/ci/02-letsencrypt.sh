a06897fc30b90c6c0edfd9638b92a07beebbe661
23t-machine-user <85880907+23t-machine-user@users.noreply.github.com>
2022-04-04 18:56:50 +0200

    Update Helm release cloudprofiles to v0.1.1 (#105)
    * Post upgrade-task to update 23ke-base-substitutions.yaml (#94)
    
    Signed-off-by: Malte Münch <muench@23technologies.cloud>
    
    * Make post upgrade script executable
    
    Signed-off-by: Malte Münch <muench@23technologies.cloud>
    
    * chdir
    
    Signed-off-by: Malte Münch <muench@23technologies.cloud>
    
    * Increase wait-timeout for cert-manager
    
    Signed-off-by: Malte Münch <muench@23technologies.cloud>
    
    * Update Helm release cloudprofiles to v0.1.1
    
    Co-authored-by: Malte Münch <muench@23technologies.cloud>
    Co-authored-by: 23t-renovate <renovate@23technologies.cloud>

--- hack/ci/02-letsencrypt.sh
+++ hack/ci/02-letsencrypt.sh
@@ -6,10 +6,10 @@ echo -n "Deploying Letsencrypt"
 # Install and configure cert-manger
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.3/cert-manager.yaml > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while deploying cert-manager ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 echo -n "."
-kubectl wait -n cert-manager deploy cert-manager-webhook --for=condition=Available > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for  cert-manager-webhook ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+kubectl wait -n cert-manager deploy cert-manager-webhook --for=condition=Available --timeout=5m > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for  cert-manager-webhook ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 echo -n "."
-kubectl wait -n cert-manager deploy cert-manager --for=condition=Available > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for  cert-manager ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+kubectl wait -n cert-manager deploy cert-manager --for=condition=Available --timeout=5m > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for  cert-manager ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 echo -n "."
-kubectl wait -n cert-manager deploy cert-manager-cainjector --for=condition=Available > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for  cert-manager-cainjector ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+kubectl wait -n cert-manager deploy cert-manager-cainjector --for=condition=Available --timeout=5m > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for  cert-manager-cainjector ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 kubectl apply -f hack/ci/misc/letsencrypt.yaml > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while deploying cluster-issuer crd ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 echo -e "\rLetsencrypt ready    ✅                       "
