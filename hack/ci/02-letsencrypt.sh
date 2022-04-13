a3473ea0f6674f5c9271b62a21aa19e26bc88b81
Malte Münch <muench@23technologies.cloud>
2022-04-13 13:30:46 +0200

    feat/minio local ca (#119)
    * Add self-signed CA to mclis accepted CAs
    
    Signed-off-by: Malte Münch <muench@23technologies.cloud>
    
    * Add additional configmap for local-ca.pem
    
    Signed-off-by: Malte Münch <muench@23technologies.cloud>
    
    * Add local-ca.pem to flux-source controller
    
    Signed-off-by: Malte Münch <muench@23technologies.cloud>
    
    * Add new clusterissuer local-ca with static CA secret
    
    Signed-off-by: Malte Münch <muench@23technologies.cloud>
    
    * Add local-ca secret and .pem
    
    Signed-off-by: Malte Münch <muench@23technologies.cloud>
    
    * Source minios certificate through local-ca clusterissuer by default
    
    Signed-off-by: Malte Münch <muench@23technologies.cloud>
    
    * Create local-ca secret
    
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/ci/02-letsencrypt.sh
+++ hack/ci/02-letsencrypt.sh
@@ -11,5 +11,9 @@ echo -n "."
 kubectl wait -n cert-manager deploy cert-manager --for=condition=Available --timeout=5m > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for  cert-manager ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 echo -n "."
 kubectl wait -n cert-manager deploy cert-manager-cainjector --for=condition=Available --timeout=5m > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for  cert-manager-cainjector ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
-kubectl apply -f hack/ci/misc/letsencrypt.yaml > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while deploying cluster-issuer crd ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+echo -n "."
+# Create secret for local-ca clusterissuer
+kubectl apply -f hack/ci/misc/local-ca-secret.yaml > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while creating local-ca secret ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+echo -n "."
+kubectl apply -f hack/ci/misc/letsencrypt.yaml > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while deploying cluster-issuer ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 echo -e "\rLetsencrypt ready    ✅                       "
