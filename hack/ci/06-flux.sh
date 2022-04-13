4f1ff589c1c5b9ba3e91823dc10011e40a2c1278
Malte Münch <muench@23technologies.cloud>
2022-04-13 12:12:44 +0200

    Add additional configmap for local-ca.pem
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/ci/06-flux.sh
+++ hack/ci/06-flux.sh
@@ -19,6 +19,13 @@ then
 fi
 echo -n "."
 
+# Alternatively there is a local CA for signing minio
+if ! kubectl -n flux-system get configmap local-ca > /tmp/stdout 2> /tmp/stderr
+then
+	kubectl create configmap local-ca -n flux-system --from-file=local-ca.pem=hack/ci/misc/local-ca.pem > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while creating local-ca.pem configmap ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+fi
+echo -n "."
+
 kubectl patch -n flux-system deployment source-controller --patch-file hack/ci/misc/flux-source-controller.patch > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while adding le-staging cert to source-controller deployment. ❌" ; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 echo -n "."
 echo -e "\rFlux installed       ✅                  "
