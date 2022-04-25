055006074ad4facc2bc35707d9e15f3c42203fb4
Fynn Späker <spaeker@23technologies.cloud>
2022-04-25 09:43:51 +0200

    Delete Shoot instead of only annote it
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>

--- hack/ci/delete-microservice-shoot.sh
+++ hack/ci/delete-microservice-shoot.sh
@@ -6,8 +6,9 @@ echo -e -n "\rDelete Microservie"
 kubectl delete -n default -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/main/release/kubernetes-manifests.yaml --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml --wait > /tmp/stdout 2> /tmp/stderr || { echo -e "\rUnsuccessful microservice deletion           ❌               "; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
 echo -n "."
 
-echo -n -e "\rDeleting shoot: 0%        "
+echo -n -e "\rDeleting shoot:           "
 kubectl annotate shoot -n garden-testing microservice confirmation.gardener.cloud/deletion=true --context garden > /tmp/stdout 2> /tmp/stderr || { echo "Error while setting shoot-deletion annotation ❌"; echo "STDOUT:"; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
-sleep 300
+
+kubectl delete shoot -n garden-testing microservice --wait=true --context garden > /tmp/stdout 2> /tmp/stderr || { echo -e "Shoot deletion unsuccessful ❌"; exit 1; }
 
 echo -e "\rDemo-Shoot deleted✅                "
