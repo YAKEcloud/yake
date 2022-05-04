bc0a35a2a53b18e4cc896103e93294b6a54d24ac
Fynn Späker <spaeker@23technologies.cloud>
2022-05-04 14:52:08 +0200

    Use nw gardener project
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>

--- hack/ci/01-shoot.sh
+++ hack/ci/01-shoot.sh
@@ -5,10 +5,10 @@ export KUBECONFIG=hack/ci/secrets/gardener-kubeconfig.yaml
 # Wait for shoot to become available
 echo -n "Creating shoot: 0%"
 OLD_PERCENTAGE=0
-while [ ! "$(kubectl get shoot $SHOOT -n garden-23t-test -o jsonpath="{.status.lastOperation.state}")" == "Succeeded" ]
+while [ ! "$(kubectl get shoot $SHOOT -n garden-23ke-ci -o jsonpath="{.status.lastOperation.state}")" == "Succeeded" ]
 do
     sleep 5
-    PERCENTAGE=$(kubectl get shoot -n garden-23t-test $SHOOT -o jsonpath="{.status.lastOperation.progress}")
+    PERCENTAGE=$(kubectl get shoot -n garden-23ke-ci $SHOOT -o jsonpath="{.status.lastOperation.progress}")
     if [ $OLD_PERCENTAGE -le $PERCENTAGE ]
     then
         echo -n -e "\rCreating shoot: $PERCENTAGE%"
@@ -17,5 +17,24 @@ do
 	sleep 1
 done
 # Get shoot kubeconfig
-kubectl get secret -n garden-23t-test $SHOOT.kubeconfig -o go-template='{{.data.kubeconfig|base64decode}}' > hack/ci/secrets/shoot-kubeconfig.yaml 2> /tmp/stderr || { echo -e "\rshoot creation unsuccessful            ❌               "; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+kubectl get secret -n garden-23ke-ci $SHOOT.kubeconfig -o go-template='{{.data.kubeconfig|base64decode}}' > hack/ci/secrets/shoot-kubeconfig.yaml 2> /tmp/stderr || { echo -e "\rshoot creation unsuccessful            ❌               "; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+export KUBECONFIG=hack/ci/secrets/shoot-kubeconfig.yaml
+
+# Wait for Toolchain (flux/minio/letsencrypt)
+kubectl wait ks -n flux-system main-ks --for=condition=ready --timeout=10m > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for kustomization toolchain(main-ks) ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+kubectl wait hr -n default secrets --for=condition=ready --timeout=10m > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for helmrelease secrets ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+kubectl wait hr -n default minio --for=condition=ready --timeout=10m > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for helmrelease minio ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+kubectl wait hr -n default cert-manager-manifest --for=condition=ready --timeout=10m > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for helmrelease cert-manager-manifest ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+kubectl wait hr -n default letsencrypt --for=condition=ready --timeout=10m > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while waiting for helmrelease letsencrypt ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
+
+# Get MinIO password
+export MINIO_PW=$(kubectl get secret -n secrets minio-pw -o go-template='{{.data.PASSWORD|base64decode}}' --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml)
+export MC_HOST_$MC_ALIAS=https://minio:$MINIO_PW@$MINIO_HOSTNAME
+
+cat << EOF >> hack/ci/handy.sh
+export MINIO_PW=$MINIO_PW
+export MC_HOST_$MC_ALIAS=https://minio:$MINIO_PW@$MINIO_HOSTNAME
+EOF
+
+
 echo -e "\rshoot ready          ✅               "
