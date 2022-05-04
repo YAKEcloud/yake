2824506727c6b29c132cf0e729ff1b4210cb1142
Fynn Späker <spaeker@23technologies.cloud>
2022-05-04 14:53:27 +0200

    Remove flux install
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>

--- hack/ci/06-flux.sh
+++ hack/ci/06-flux.sh
@@ -2,17 +2,6 @@
 
 source hack/ci/handy.sh
 
-#if [[ $CI == "true" ]]
-#then
-#    FLUX=/usr/local/bin/flux
-#else
-#    FLUX=$(which flux)
-#fi
-# Install flux
-#echo -n "Installing Flux"
-#$FLUX install > /tmp/stdout 2> /tmp/stderr || { echo -e "\rError while installing Flux ❌"; echo "STDOUT":; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
-#echo -n "."
-
 # We are using letsencrypt staging for testing purposes
 if ! kubectl -n flux-system get configmap le-staging > /tmp/stdout 2> /tmp/stderr
 then
