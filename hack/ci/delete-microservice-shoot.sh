4b5a06ff8e5ae956b6017fd3bb6adb2ab136b79d
Lothar Gesslein <gesslein@23technologies.cloud>
2022-06-23 12:29:12 +0200

    more cleanup

--- hack/ci/delete-microservice-shoot.sh
+++ hack/ci/delete-microservice-shoot.sh
@@ -1,10 +1,9 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 source hack/ci/handy.sh
 
-echo -n -e "\rDeleting shoot:           "
-kubectl annotate shoot -n garden-testing microservice confirmation.gardener.cloud/deletion=true --context garden > /tmp/stdout 2> /tmp/stderr || { echo "Error while setting shoot-deletion annotation ❌"; echo "STDOUT:"; cat /tmp/stdout; echo "STDERR:"; cat /tmp/stderr; exit 1; }
-
-kubectl delete shoot -n garden-testing microservice --wait=true --context garden > /tmp/stdout 2> /tmp/stderr || { echo -e "Shoot deletion unsuccessful ❌"; exit 1; }
-
-echo -e "\rDemo-Shoot deleted✅                "
+echo "Deleting demo shoot"
+kubectl annotate shoot -n garden-testing microservice confirmation.gardener.cloud/deletion=true --context garden --overwrite=true
+kubectl delete shoot -n garden-testing microservice --wait=true --context garden
+echo "Demo shoot deleted ✅"
