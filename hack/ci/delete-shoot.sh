f19244276a57a9f9222ccf3d317b757bdb96d753
Lothar Gesslein <gesslein@23technologies.cloud>
2022-06-23 12:53:15 +0200

    Correct and expand on ci workflow in readme

--- hack/ci/delete-shoot.sh
+++ hack/ci/delete-shoot.sh
@@ -4,10 +4,6 @@ set -euo pipefail
 source hack/ci/handy.sh
 
 echo "Deleting shoot"
-
-rm hack/ci/secrets/shoot-kubeconfig.yaml 2> /dev/null
-rm hack/ci/secrets/apiserver-in-shoot-kubeconfig.yaml 2> /dev/null
-
 export KUBECONFIG=hack/ci/secrets/gardener-kubeconfig.yaml
 kubectl annotate shoot "$SHOOT" confirmation.gardener.cloud/deletion=true --overwrite=true
 kubectl delete shoot "$SHOOT" --wait=false
