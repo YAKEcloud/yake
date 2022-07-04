fe54e3864cc0005fd37dd27bffbd63c3202a0825
Malte Münch <muench@23technologies.cloud>
2022-07-04 14:52:58 +0200

    Enable local charts folder (#216)
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/ci/02-23ke-bucket.sh
+++ hack/ci/02-23ke-bucket.sh
@@ -22,4 +22,17 @@ grep -lr GitRepository flux/ | while IFS= read -r file; do
     sed s/GitRepository/Bucket/ "$file" | mc pipe -q "$MC_ALIAS/$BUCKET/$file"
 done
 
+# In case you want to use a local version of the 23ke-charts, you can clone the 23ke-charts repository
+# to the folder "23ke-charts-dev", this script will detect the folder and use the local
+# version instead. that way you can quickly alter the charts to use a development image or
+# anything else youd like to change quickly.
+if [[ -d 23ke-charts-dev ]]
+then
+    echo "Using local charts-folder  ./23ke-charts-dev"
+    for file in $(grep -lr 23ke-charts base-install); do
+            cat $file | yq eval '.spec.chart.spec |= (.chart = "./23ke-charts-dev/" + .chart | del .version) |= .sourceRef |= (.kind = "Bucket" | .name  = "23ke" )' - |   mc pipe $MC_ALIAS/$BUCKET/$file
+    done
+    mc cp -q --recursive 23ke-charts-dev/charts/ $MC_ALIAS/$BUCKET/23ke-charts-dev/
+fi
+
 echo "23KE Bucket ready ✅"
