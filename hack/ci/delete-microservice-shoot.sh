f772141ce5652c1fce3269905df81b00e389e08c
Fynn Späker <spaeker@23technologies.cloud>
2022-09-12 14:53:26 +0200

    refine-deletion-of-microservice (#290)
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>
    
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>

--- hack/ci/delete-microservice-shoot.sh
+++ hack/ci/delete-microservice-shoot.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 source hack/ci/handy.sh
 
+echo "Deleting microservice"
+kubectl delete -n default -f hack/ci/misc/microservices-demo.yaml --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml --wait
+
 echo "Deleting demo shoot"
 kubectl annotate shoot -n garden-testing microservice confirmation.gardener.cloud/deletion=true --context garden --overwrite=true
 kubectl delete shoot -n garden-testing microservice --wait=true --context garden
