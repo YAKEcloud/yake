08258ccde016daf923e89e799ab85643d2fff7ae
Jan Lohage <github@j2L4e.de>
2022-11-04 11:26:18 +0100

    Don't exit on errors

--- hack/ci/delete-microservice-shoot.sh
+++ hack/ci/delete-microservice-shoot.sh
@@ -4,9 +4,8 @@ set -euo pipefail
 source hack/ci/handy.sh
 
 echo "Deleting microservice"
-kubectl delete -n default -f hack/ci/misc/microservices-demo.yaml --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml --wait
+kubectl delete -n default -f hack/ci/misc/microservices-demo.yaml --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml --wait || echo "There was an error deleting the microservice"
 
 echo "Deleting demo shoot"
-kubectl annotate shoot -n garden-testing microservice confirmation.gardener.cloud/deletion=true --context garden --overwrite=true
-kubectl delete shoot -n garden-testing microservice --wait=true --context garden
-echo "Demo shoot deleted ✅"
+kubectl annotate shoot -n garden-testing microservice confirmation.gardener.cloud/deletion=true --context garden --overwrite=true || echo "There was an error annotating the demo shoot"
+kubectl delete shoot -n garden-testing microservice --wait=true --context garden || echo "There was an error deleting the demo shoot"
