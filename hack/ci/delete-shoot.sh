09a269af5bc2c1070856bacfa64e181059aa84aa
Jan Lohage <github@j2L4e.de>
2022-11-04 11:26:18 +0100

    Don't exit on errors

--- hack/ci/delete-shoot.sh
+++ hack/ci/delete-shoot.sh
@@ -4,11 +4,10 @@ set -euo pipefail
 source hack/ci/handy.sh
 
 # delete buckets on azure
-rclone -q purge $REMOTE:$BUCKET
-rclone -q purge $REMOTE:$CONFIG_BUCKET
+rclone -q purge $REMOTE:$BUCKET || echo "Deleting bucket failed"
+rclone -q purge $REMOTE:$CONFIG_BUCKET || echo "Deleting config bucket failed"
 
 echo "Deleting shoot"
 export KUBECONFIG=hack/ci/secrets/gardener-kubeconfig.yaml
-kubectl annotate shoot "$SHOOT" confirmation.gardener.cloud/deletion=true --overwrite=true
-kubectl delete shoot "$SHOOT" --wait=false
-echo "Shoot will be deleted in the background ✅"
+kubectl annotate shoot "$SHOOT" confirmation.gardener.cloud/deletion=true --overwrite=true || echo "Annotating shoot failed"
+kubectl delete shoot "$SHOOT" --wait=false || echo "Deleting shoot failed"
