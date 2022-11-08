fbc5249ba7af4544bc0502167e1a578132f08db5
Jens Schneider <schneider@23technologies.cloud>
2022-11-08 11:46:40 +0100

    Fix some issues with the new design
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

--- hack/ci/04-23ke.sh
+++ hack/ci/04-23ke.sh
@@ -34,7 +34,7 @@ stringData:
       email: operations@23technologies.cloud
       staticPassword:
         enabled: true
-        hash: "$2a$10$2b2cU8CPhOTaGrs1HRQuAueS7JTT5ZHsHSzYiFPm1leZck7Mc8T4W"
+        hash: "\$2a\$10\$2b2cU8CPhOTaGrs1HRQuAueS7JTT5ZHsHSzYiFPm1leZck7Mc8T4W"
         userID: 08a8684b-db88-4b73-90a9-3cd1661f5466
 
     issuer:
