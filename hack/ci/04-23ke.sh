46a3d97ebe594f3e759427f7162879152851f5aa
Jan Lohage <j2L4e@users.noreply.github.com>
2022-11-11 11:02:45 +0100

    Display more info on errors (#381)

--- hack/ci/04-23ke.sh
+++ hack/ci/04-23ke.sh
@@ -3,6 +3,20 @@ set -euo pipefail
 
 source hack/ci/handy.sh
 
+dumpHr() {
+  local helmreleases=$(kubectl get helmreleases -n flux-system | sed 1,1d | awk '{ if($3 != "True") print $1 } | xargs echo')
+  kubectl get hr $helmreleases -n flux-system -o wide
+  echo ''
+  kubectl get hr $helmreleases -n flux-system -o yaml
+}
+
+dumpKs() {
+  local kustomizations=$(kubectl get kustomizations -n flux-system | sed 1,1d | awk '{ if($3 != "True") print $1 } | xargs echo')
+  kubectl get kustomizations $kustomizations -n flux-system -o wide
+  echo ''
+  kubectl get kustomizations $kustomizations -n flux-system -o yaml
+}
+
 echo "Installing 23KE"
 
 # Templating 23ke-config.yaml
@@ -77,11 +91,12 @@ flux create source bucket 23ke-config --endpoint=https://23ketestbed.blob.core.w
 kubectl apply -f hack/ci/flux
 
 echo "Waiting for ks"
-kubectl wait kustomization 23ke-base -n flux-system --for=condition=ready --timeout=10m
-kubectl wait kustomization 23ke-env -n flux-system --for=condition=ready --timeout=10m
-kubectl wait kustomization -A --all --for=condition=ready --timeout=10m
+kubectl wait kustomization 23ke-base -n flux-system --for=condition=ready --timeout=10m || { dumpKs; exit 1; }
+kubectl wait kustomization 23ke-env -n flux-system --for=condition=ready --timeout=10m  || { dumpKs; exit 1; }
+kubectl wait kustomization -A --all --for=condition=ready --timeout=10m || { dumpKs; exit 1; }
+
 echo "Waiting for helmreleases"
-kubectl wait helmrelease -A --all --for=condition=ready --timeout=20m
+kubectl wait helmrelease -A --all --for=condition=ready --timeout=20m || { dumpHr; exit 1; }
 
 # Extract apiserver-in-shoot-kubeconfig.yaml
 kubectl get secrets -n garden garden-kubeconfig-for-admin -o go-template='{{.data.kubeconfig | base64decode }}' > hack/ci/secrets/apiserver-in-shoot-kubeconfig.yaml
