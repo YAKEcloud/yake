5b2f0971b45dee06b2485350ed4c501912225e17
Jan Lohage <github@j2L4e.de>
2022-12-20 13:26:39 +0100

    naively enforce correct working directory
    Signed-off-by: Jan Lohage <github@j2L4e.de>

--- hack/prepare-release.sh
+++ hack/prepare-release.sh
@@ -7,6 +7,11 @@ versionPattern='^[0-9]{1,}[.][0-9]{1,}[.][0-9]{1,}-[0-9]{1,}$'
 
 version=$1
 
+if [[ ! -d .git ]]; then
+  echo "script must be run from the repository root"
+  exit 1
+fi
+
 if [[ ! $version =~ $versionPattern ]]; then
   echo "usage: $0 1.2.3-4"
   exit 1
