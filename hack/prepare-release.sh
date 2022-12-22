f3ae92ad6c8bb90704a870e18f0c50281c9ce7c0
Jan Lohage <j2L4e@users.noreply.github.com>
2022-12-22 09:19:48 +0100

    Add script+action for handling git-side of release process (#467)
    Signed-off-by: Jan Lohage <lohage@23technologies.cloud>

--- hack/prepare-release.sh
+++ hack/prepare-release.sh
@@ -2,25 +2,69 @@
 
 set -eo pipefail
 
+_fail() {
+  msg=$1
+  prefix=""
+  if [ "$GITHUB_ACTIONS" == "true" ]; then
+    prefix="::error::"
+  fi
+
+  echo "$prefix$msg"
+  exit 1
+}
+
 minorPattern='^[0-9]{1,}[.][0-9]{1,}'
 versionPattern='^[0-9]{1,}[.][0-9]{1,}[.][0-9]{1,}-[0-9]{1,}$'
 
-version=$1
+version=${version:-$1}
 
 if [[ ! -d .git ]]; then
-  echo "script must be run from the repository root"
-  exit 1
+  _fail "script must be run from the repository root"
 fi
 
 if [[ ! $version =~ $versionPattern ]]; then
-  echo "usage: $0 1.2.3-4"
-  exit 1
+  _fail "usage: $0 1.2.3-4"
 fi
 
-minor=$(echo "$version" | grep -nE "$minorPattern")
-echo "Updating chart versions to $0"
-for chart in $(find . -name Chart.yaml); do
-  echo $chart
+minor=$(echo "$version" | grep -oE "$minorPattern")
+releaseNotes="release-notes/v$minor.md"
+
+tag="v$version"
+branch="release-v$minor"
+
+git fetch
+git fetch --tags
+
+if [[ -e ".git/refs/tags/$tag" ]]; then
+  _fail "A tag named '$tag' already exists."
+fi
+
+if [[ ! -f "$releaseNotes" ]]; then
+  _fail "No release notes found for version $minor. Expected file '$releaseNotes' to exist."
+fi
+
+if [[ $(grep "$branch" .github/renovate.json5 | wc -l) == "0" ]]; then
+  # todo: update renovate.json5 automatically
+
+  _fail "It seems you haven't set up .github/renovate.json5 to track the new version's release branch ($branch). Please do so."
+fi
+
+if [[ -e ".git/refs/remotes/origin/$branch" ]]; then
+  git switch "$branch"
+  git pull
+else
+  git switch -c "$branch"
+fi
+
+echo "Updating chart versions to $version"
+while read -r chart; do
+  echo "$chart"
   sed -i -r -e "s;^version:.+$;version: $version;" "$chart"
-done
+done <<< "$(find . -name Chart.yaml)"
 echo "Done"
+
+git add .
+git commit -m "$tag"
+git tag "$tag"
+git push -u origin "$branch"
+git push -u origin "$tag"
