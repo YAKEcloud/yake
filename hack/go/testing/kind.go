79a9e6621afb94af2f9b484aab069bf3cd704aae
Jens Schneider <schneider@23technologies.cloud>
2023-01-31 16:44:48 +0100

    Finish kind cluster preparation for tests
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

--- hack/go/testing/kind.go
+++ hack/go/testing/kind.go
@@ -3,18 +3,29 @@ package testing
 import (
 	"context"
 	"fmt"
-	"github.com/23technologies/23kectl/pkg/common"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/util/wait"
+	"io"
+	"log"
+	"net"
+	"net/http"
 	"os"
-	"os/exec"
 	"path"
-	client2 "sigs.k8s.io/controller-runtime/pkg/client"
 	"time"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
+	"github.com/23technologies/23kectl/pkg/common"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	metallbv1beta1 "go.universe.tf/metallb/api/v1beta1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	dockertypes "github.com/docker/docker/api/types"
+	dockerclient "github.com/docker/docker/client"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"sigs.k8s.io/kind/pkg/cluster"
@@ -45,42 +56,47 @@ func useKindCluster(ctx SpecContext, version string) (string, string) {
 	Expect(clusterProvider.List()).To(ContainElement(name))
 	Expect(kubeconfig).To(BeAnExistingFile())
 
-	dockerClient, err := client.NewClientWithOpts(client.FromEnv)
-	dockerNetwork, err := dockerClient.NetworkInspect(context.Background(), "kind", types.NetworkInspectOptions{})
-	dockerNodeCidr := dockerNetwork.IPAM.Config[0].Subnet
-
-	fmt.Println(dockerNodeCidr)
-
-	//ip, _, err := net.ParseCIDR(dockerNodeCidr)
-	//ipRangeEnd := make(net.IP, 4)
-	//copy(ipRangeEnd, ip.To4())
-	//ipRangeEnd[3] += 5
-	//
-	//ipAddressPool := metallbv1beta1.IPAddressPool{
-	//	ObjectMeta: metav1.ObjectMeta{
-	//		Name:      "address-pool",
-	//		Namespace: "metallb-system"},
-	//	Spec: metallbv1beta1.IPAddressPoolSpec{Addresses: []string{ip.String() + "-" + ipRangeEnd.String()}},
-	//}
-	//
-	//l2Advertisement := metallbv1beta1.L2Advertisement{
-	//	ObjectMeta: metav1.ObjectMeta{
-	//		Name:      "empty",
-	//		Namespace: "metallb-system"},
-	//}
-	//
-	//_ = ipAddressPool
-	//_ = l2Advertisement
+	// create the k8s client
 	_, _, k8sClient, err := common.CreateKubeClient(kubeconfig)
 	Expect(err).NotTo(HaveOccurred())
 
-	// todo: get rid of dependency to kubectl
-	cmd := exec.Command("kubectl", "apply", "--kubeconfig="+kubeconfig, "-f", "https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Run()
+	err = metallbv1beta1.AddToScheme(k8sClient.Scheme())
 	Expect(err).NotTo(HaveOccurred())
 
+	// dowload and apply metallb-native.yaml
+	httpClient := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			r.URL.Opaque = r.URL.Path
+			return nil
+		},
+	}
+	resp, err := httpClient.Get("https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	decoder := yaml.NewYAMLOrJSONDecoder(resp.Body, 1024)
+	var decodedObj map[string]interface{}
+
+	for indexInFile := 0; true; indexInFile++ {
+		err := decoder.Decode(&decodedObj)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			continue
+		}
+		if decodedObj == nil {
+			continue
+		}
+
+		obj := &unstructured.Unstructured{Object: decodedObj}
+		_, err = controllerutil.CreateOrUpdate(context.Background(), k8sClient, obj, func() error { return nil })
+		Expect(err).NotTo(HaveOccurred())
+	}
+
+	// wait for the metallb pods to be ready
 	err = wait.PollUntilWithContext(ctx, time.Second, func(ctx context.Context) (bool, error) {
 		var err error
 
@@ -89,8 +105,8 @@ func useKindCluster(ctx SpecContext, version string) (string, string) {
 			return false, err
 		}
 
-		list := &v1.PodList{}
-		err = k8sClient.List(ctx, list, &client2.ListOptions{
+		list := &corev1.PodList{}
+		err = k8sClient.List(ctx, list, &k8sclient.ListOptions{
 			LabelSelector: ls,
 			FieldSelector: nil,
 			Namespace:     "metallb-system",
@@ -110,7 +126,6 @@ func useKindCluster(ctx SpecContext, version string) (string, string) {
 					continue OUTER
 				}
 			}
-
 			return false, nil
 		}
 
@@ -118,10 +133,35 @@ func useKindCluster(ctx SpecContext, version string) (string, string) {
 	})
 	Expect(err).NotTo(HaveOccurred())
 
-	cmd = exec.Command("kubectl", "apply", "--kubeconfig="+kubeconfig, "-f", "metallb-config.yaml")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Run()
+	// find out the cidr of the kind docker network and use some
+	// ip addresses from that network for loadbalancers
+	dockerClient, err := dockerclient.NewClientWithOpts(dockerclient.FromEnv)
+	dockerNetwork, err := dockerClient.NetworkInspect(context.Background(), "kind", dockertypes.NetworkInspectOptions{})
+	dockerNodeCidr := dockerNetwork.IPAM.Config[0].Subnet
+
+	ip, _, err := net.ParseCIDR(dockerNodeCidr)
+	ipRangeEnd := make(net.IP, 4)
+	copy(ipRangeEnd, ip.To4())
+	ipRangeEnd[3] += 5
+
+	// create metallb objects.
+	// See also https://kind.sigs.k8s.io/docs/user/loadbalancer/
+	ipAddressPool := metallbv1beta1.IPAddressPool{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "address-pool",
+			Namespace: "metallb-system"},
+		Spec: metallbv1beta1.IPAddressPoolSpec{Addresses: []string{ip.String() + "-" + ipRangeEnd.String()}},
+	}
+
+	l2Advertisement := metallbv1beta1.L2Advertisement{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "empty",
+			Namespace: "metallb-system"},
+	}
+
+	_, err = controllerutil.CreateOrUpdate(context.Background(), k8sClient, &ipAddressPool, func() error { return nil })
+	Expect(err).NotTo(HaveOccurred())
+	_, err = controllerutil.CreateOrUpdate(context.Background(), k8sClient, &l2Advertisement, func() error { return nil })
 	Expect(err).NotTo(HaveOccurred())
 
 	return kubeconfig, name
