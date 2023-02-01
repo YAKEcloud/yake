7040843157e3d7d7465dc5aac364d639d1d58c9b
Lothar <gesslein@23technologies.cloud>
2023-02-01 11:18:59 +0100

    use randomness from 23ke-run-... in microservice/test shoot name (#518)

--- hack/ci/06-tests.sh
+++ hack/ci/06-tests.sh
@@ -17,43 +17,28 @@ kubectl wait $FIRSTBUCKET --for=jsonpath='{.status.lastOperation.state}'=Succeed
 
 
 echo "Deploying sample Shoot"
+export SHOOTNAME="test-${SHOOT#23ke-run-}"
 
-kubectl apply -f hack/ci/misc/shoot-microservice-"$PROVIDER"-"$ZONE".yaml --context garden
+echo "Deploying test Shoot"
+yq eval '.metadata.name = env(SHOOTNAME)' hack/ci/misc/shoot-microservice-"$PROVIDER"-"$ZONE".yaml | kubectl --context garden apply -f -
 
 # Wait for shoot to become available
-echo "Waiting for shoot creation..."
+echo "Waiting for shoot $SHOOTNAME creation..."
 
-MAX_WAIT=300
-PERCENTAGE=0
-LASTPERCENTAGE=0
-LASTCHANGE=$(date +%s)
-while [ ! "$(kubectl --context garden get shoot microservice -n garden-testing -o jsonpath="{.status.lastOperation.state}")" == "Succeeded" ]; do
-  NOW=$(date +%s)
-  LASTPERCENTAGE=$PERCENTAGE
-  PERCENTAGE=$(kubectl --context garden get shoot microservice -n garden-testing -o jsonpath="{.status.lastOperation.progress}")
-
-  if [ "$LASTPERCENTAGE" -ne "$PERCENTAGE" ]; then
-    LASTCHANGE=$NOW
-  fi
-  
-  if [ "$(( $NOW - $LASTCHANGE ))" -ge "$MAX_WAIT" ]; then
-    echo "No progress in $MAX_WAIT seconds. Exiting."
-    
-    kubectl --context garden get shoot microservice -n garden-testing -o yaml; exit 1;
-  fi
-  
+while [ ! "$(kubectl --context garden get shoot "$SHOOTNAME" -n garden-testing -o jsonpath="{.status.lastOperation.state}")" == "Succeeded" ]; do
+  PERCENTAGE=$(kubectl --context garden get shoot "$SHOOTNAME" -n garden-testing -o jsonpath="{.status.lastOperation.progress}")
   echo "Creating shoot: $PERCENTAGE%"
 	sleep 5
 done
 echo "Shoot creation succeeded"
 
-kubectl get secret -n garden-testing microservice.kubeconfig -o go-template='{{.data.kubeconfig|base64decode}}' --context garden > hack/ci/secrets/shoot-microservice-kubeconfig.yaml
+kubectl get secret -n garden-testing "$SHOOTNAME".kubeconfig -o go-template='{{.data.kubeconfig|base64decode}}' --context garden > hack/ci/secrets/shoot-microservice-kubeconfig.yaml
 
 echo "Deploying microservice demo"
 kubectl apply -n default -f hack/ci/misc/microservices-demo.yaml --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml
 echo "Waiting for microservice demo"
 sleep 5
 kubectl wait --for=condition=available --timeout=600s -n default deployment/loadgenerator --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml \
-  || { kubectl --context garden get shoot microservice -n garden-testing -o yaml; exit 1; }
+  || { kubectl --context garden get shoot "$SHOOTNAME" -n garden-testing -o yaml; exit 1; }
 
 echo -e "Demo-Deployment ready ✅"
--- hack/ci/delete-microservice-shoot.sh
+++ hack/ci/delete-microservice-shoot.sh
@@ -3,9 +3,11 @@ set -euo pipefail
 
 source hack/ci/handy.sh
 
+SHOOTNAME="test-${SHOOT#23ke-run-}"
+
 echo "Deleting microservice"
 kubectl delete -n default -f hack/ci/misc/microservices-demo.yaml --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml --wait || echo "There was an error deleting the microservice"
 
-echo "Deleting demo shoot"
-kubectl annotate shoot -n garden-testing microservice confirmation.gardener.cloud/deletion=true --context garden --overwrite=true || echo "There was an error annotating the demo shoot"
-kubectl delete shoot -n garden-testing microservice --wait=true --context garden || echo "There was an error deleting the demo shoot"
+echo "Deleting shoot $SHOOTNAME"
+kubectl annotate shoot -n garden-testing "$SHOOTNAME" confirmation.gardener.cloud/deletion=true --context garden --overwrite=true || echo "There was an error annotating the test shoot"
+kubectl delete shoot -n garden-testing "$SHOOTNAME" --wait=true --context garden || echo "There was an error deleting the test shoot"
