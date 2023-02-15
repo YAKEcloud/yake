677e553d32d92755c1931d7dce4a2a1d2c928f9b
Jan Lohage <lohage@23technologies.cloud>
2023-02-15 10:37:17 +0100

    use garden-dev namespace for shoot
    Signed-off-by: Jan Lohage <lohage@23technologies.cloud>

--- hack/ci/06-tests.sh
+++ hack/ci/06-tests.sh
@@ -26,20 +26,22 @@ yq eval '.metadata.name = env(SHOOTNAME)' hack/ci/misc/shoot-microservice-"$PROV
 # Wait for shoot to become available
 echo "Waiting for shoot $SHOOTNAME creation..."
 
-while [ ! "$(kubectl --context garden get shoot "$SHOOTNAME" -n garden-testing -o jsonpath="{.status.lastOperation.state}")" == "Succeeded" ]; do
-  PERCENTAGE=$(kubectl --context garden get shoot "$SHOOTNAME" -n garden-testing -o jsonpath="{.status.lastOperation.progress}")
+SHOOTNAMESPACE=garden-dev
+
+while [ ! "$(kubectl --context garden get shoot "$SHOOTNAME" -n "$SHOOTNAMESPACE" -o jsonpath="{.status.lastOperation.state}")" == "Succeeded" ]; do
+  PERCENTAGE=$(kubectl --context garden get shoot "$SHOOTNAME" -n "$SHOOTNAMESPACE" -o jsonpath="{.status.lastOperation.progress}")
   echo "Creating shoot: $PERCENTAGE%"
 	sleep 5
 done
 echo "Shoot creation succeeded"
 
-kubectl get secret -n garden-testing "$SHOOTNAME".kubeconfig -o go-template='{{.data.kubeconfig|base64decode}}' --context garden > hack/ci/secrets/shoot-microservice-kubeconfig.yaml
+kubectl get secret -n "$SHOOTNAMESPACE" "$SHOOTNAME".kubeconfig -o go-template='{{.data.kubeconfig|base64decode}}' --context garden > hack/ci/secrets/shoot-microservice-kubeconfig.yaml
 
 echo "Deploying microservice demo"
 kubectl apply -n default -f hack/ci/misc/microservices-demo.yaml --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml
 echo "Waiting for microservice demo"
 sleep 5
 kubectl wait --for=condition=available --timeout=600s -n default deployment/loadgenerator --kubeconfig hack/ci/secrets/shoot-microservice-kubeconfig.yaml \
-  || { kubectl --context garden get shoot "$SHOOTNAME" -n garden-testing -o yaml; exit 1; }
+  || { kubectl --context garden get shoot "$SHOOTNAME" -n "$SHOOTNAMESPACE" -o yaml; exit 1; }
 
 echo -e "Demo-Deployment ready ✅"
