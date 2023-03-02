717947387495f98a2666ee5ce0a51f20d01d3ca8
Jens Schneider <schneider@23technologies.cloud>
2023-03-02 14:41:29 +0100

    Make release action more homegeneous
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

--- hack/ci/02-23ke-bucket.sh
+++ hack/ci/02-23ke-bucket.sh
@@ -5,25 +5,7 @@ source hack/ci/handy.sh
 
 echo "23KE Bucket upload"
 
-tmpDir=$(mktemp -d)
-
-cp -r kustomization.yaml $tmpDir
-cp -r flux $tmpDir
-cp -r pre-gardener $tmpDir
-cp -r flux-system $tmpDir
-cp -r gardener $tmpDir
-cp -r helmcharts $tmpDir
-
-cwd=$(pwd)
-pushd $tmpDir
-
-# we now upload packet versions which use a bucket instead of the GitRepository
-for file in $(grep -lr GitRepository pre-gardener gardener flux | sed 's/^\.\///'); do
-		sed -i s/GitRepository/Bucket/ $file
-done
-
-$cwd/hack/rewrite-helmchart-sourcerefs.sh Bucket
-popd
+tmpDir=$(hack/make-tmp-release-dir.sh Bucket)
 
 rclone -q mkdir $REMOTE:$BUCKET
 rclone -q sync $tmpDir $REMOTE:$BUCKET
--- hack/make-tmp-release-dir.sh
+++ hack/make-tmp-release-dir.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 
+# create a temporary directory containing all conents needed for
+# a release. The argument to this script defines the sourceType.
+# Possbile options are:
+# - GitRepository
+# - Bucket
+# - OCIRepository
+
 sourceType=$1
+
+tmpDir=$(mktemp -d)
+
+cp -r kustomization.yaml $tmpDir
+cp -r flux $tmpDir
+cp -r pre-gardener $tmpDir
+cp -r flux-system $tmpDir
+cp -r gardener $tmpDir
+cp -r helmcharts $tmpDir
+
+cwd=$(pwd)
+cd $tmpDir
+
+# we now upload packet versions which use a bucket instead of the GitRepository
+for file in $(grep -lr GitRepository pre-gardener gardener flux | sed 's/^\.\///'); do
+		sed -i s/GitRepository/$sourceType/ $file
+done
 
 for file in $(grep  --exclude=configuration.yaml \
 										--exclude=extensions.yaml \
@@ -17,3 +41,6 @@ do
 		# add new sourceRef as GitRepository
 		sed -i -E  "s/      sourceRef:/      sourceRef:\n        kind: $sourceType\n        name: 23ke/" $file
 done
+
+cd $cwd
+echo $tmpDir
