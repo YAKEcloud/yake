9d2e585f17c518130e9694380f5ae5dff444e6d7
Jan Lohage <lohage@23technologies.cloud>
2023-03-21 13:22:53 +0100

    retry hr installs mroe often
    Signed-off-by: Jan Lohage <lohage@23technologies.cloud>

--- hack/ci/02-23ke-bucket.sh
+++ hack/ci/02-23ke-bucket.sh
@@ -8,17 +8,22 @@ echo "23KE Bucket upload"
 
 tmpDir=$(hack/make-tmp-release-dir.sh Bucket)
 
+# Speed up flux
+yq eval -i '(
+  select(.kind == "Deployment") |
+  select(
+    .metadata.name == "helm-controller" or
+    .metadata.name == "kustomize-controller"
+  ) |
+  .spec.template.spec.containers[0].args
+) += "--requeue-dependency=5s"' "$tmpDir/flux-system/gotk-components.yaml"
+
+# add spec.install.remediation.retries to all templates/yamls in /pre-gardener and /gardener that include "^kind: HelmRelease", but don't include "remediation"
+grep -rl '^kind: HelmRelease' pre-gardener gardener \
+  | xargs grep -L 'remediation' \
+  | xargs sed -i 's;^spec:;spec:\n  install:\n    remediation:\n      retries: 25;'
+
 mc ls "$MC_ALIAS/$BUCKET_23KE" &> /dev/null || mc mb "$MC_ALIAS/$BUCKET_23KE"
 mc mirror -q --overwrite "$tmpDir" "$MC_ALIAS/$BUCKET_23KE"
-
-# Speed up flux
-# yq eval '(
-#   select(.kind == "Deployment") |
-#   select(
-#     .metadata.name == "helm-controller" or
-#     .metadata.name == "kustomize-controller"
-#   ) |
-#   .spec.template.spec.containers[0].args
-# ) += "--requeue-dependency=5s"' "$tmpDir/flux-system/gotk-components.yaml" | mc pipe $MC_ALIAS/$BUCKET_23KE/flux-system/gotk-components.yaml
 
 echo "23KE Bucket ready ✅"
