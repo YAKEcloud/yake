638f65b50ccda9bf773277a92f369cf67cae5037
rhizoet <58997736+rhizoet@users.noreply.github.com>
2023-03-22 09:11:59 +0100

    Readded Privacy policy to the docs (#659)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -176,6 +176,10 @@ const config = {
                 label: "Legals",
                 to: "/legals",
               },
+              {
+                label: "Privacy policy",
+                to: "/privacy-policy",
+              },
             ],
           },
         ],
