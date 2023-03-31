691567f7231e4990120bfd80af0ddc136289c388
Jens Schneider <schneider@23technologies.cloud>
2023-03-31 17:30:15 +0200

    [skip ci] Delete ingresses and dnsentries before deleting the buckets
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

--- hack/ci/delete-shoot.sh
+++ hack/ci/delete-shoot.sh
@@ -4,18 +4,18 @@ set -euo pipefail
 source hack/ci/util.sh
 source hack/ci/handy.sh
 
-# delete buckets on azure
-rclone -q purge $REMOTE:$BACKUP_BUCKET || echo "Deleting backup bucket failed"
-
-# delete 23ke bucket
-mc rb -q --dangerous --force $MC_ALIAS/$BUCKET_23KE || echo "Deleting bucket failed"
-
 # delete ingresses and dnsentries, so that the dnsentries are not leaked on azure
 echo "Deleting dnsentries"
 kubectl delete -n garden ingress apiserver-ingress
 kubectl delete -n garden ingress gardener-dashboard-ingress
 kubectl delete -n garden ingress identity-ingress
 kubectl delete --all -n garden dnsentries.dns.gardener.cloud
+
+# delete buckets on azure
+rclone -q purge $REMOTE:$BACKUP_BUCKET || echo "Deleting backup bucket failed"
+
+# delete 23ke bucket
+mc rb -q --dangerous --force $MC_ALIAS/$BUCKET_23KE || echo "Deleting bucket failed"
 
 echo "Deleting shoot"
 export KUBECONFIG=hack/ci/secrets/gardener-kubeconfig.yaml
