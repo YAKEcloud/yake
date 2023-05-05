40e8eb70818283ee3058a5879f38fdda948d428b
Jan Lohage <lohage@23technologies.cloud>
2023-05-05 11:19:27 +0200

    fix 23kectl call
    Signed-off-by: Jan Lohage <lohage@23technologies.cloud>

--- hack/ci/04-23ke.sh
+++ hack/ci/04-23ke.sh
@@ -30,33 +30,33 @@ fi
 ./23kectl install --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml --config hack/ci/misc/23kectl-config.yaml
 
 echo "Waiting for Kustomization pre-gardener"
-kubectl wait kustomization 23ke-base -n flux-system --for=condition=ready --timeout=10m || { 23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
-kubectl wait kustomization 23ke-config -n flux-system --for=condition=ready --timeout=10m || { 23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
-kubectl wait kustomization 23ke-env-config -n flux-system --for=condition=ready --timeout=10m || { 23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
-kubectl wait kustomization pre-gardener -n flux-system --for=condition=ready --timeout=10m || { 23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
+kubectl wait kustomization 23ke-base -n flux-system --for=condition=ready --timeout=10m || { ./23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
+kubectl wait kustomization 23ke-config -n flux-system --for=condition=ready --timeout=10m || { ./23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
+kubectl wait kustomization 23ke-env-config -n flux-system --for=condition=ready --timeout=10m || { ./23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
+kubectl wait kustomization pre-gardener -n flux-system --for=condition=ready --timeout=10m || { ./23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
 
 
 echo "Waiting for Kustomization gardener"
 # sleep until hr kube-apiserver appears
 while ! kubectl get helmrelease kube-apiserver -n flux-system; do sleep 10; done
 # Wait for vgarden kube-apiserver, extract vgarden kubeconfig and upload hosting shoots kubeconfig to vgarden, a secret referenced by internal-gardenlet
-kubectl wait helmrelease kube-apiserver -n flux-system --for=condition=ready --timeout=10m || { 23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
+kubectl wait helmrelease kube-apiserver -n flux-system --for=condition=ready --timeout=10m || { ./23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
 # sleep until dnsentry appears and is ready
 while ! dnsentryname="$(kubectl get dnsentries -n garden -o name | grep apiserver-ingress-ingress)"; do sleep 1; done
 kubectl wait $dnsentryname -n garden --for=jsonpath='{.status.state}'=Ready --timeout=5m
 # wait for gardener-application so garden namespace exists
-kubectl wait helmrelease gardener-application -n flux-system --for=condition=ready --timeout=10m || { 23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
+kubectl wait helmrelease gardener-application -n flux-system --for=condition=ready --timeout=10m || { ./23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
 kubectl get secrets -n garden garden-kubeconfig-for-admin -o go-template='{{.data.kubeconfig | base64decode }}' > hack/ci/secrets/apiserver-in-shoot-kubeconfig.yaml
 
 waitForDNS "api.${SHOOT}.${AZURE_BASE_DOMAIN}"
 kubectl create secret generic hcloud-hel1-0-kubeconfig -n garden --from-file=kubeconfig=hack/ci/secrets/shoot-kubeconfig.yaml --save-config --dry-run=client -o yaml | kubectl --context garden apply -f -
 
-kubectl wait kustomization gardener -n flux-system --for=condition=ready --timeout=10m  || { 23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
+kubectl wait kustomization gardener -n flux-system --for=condition=ready --timeout=10m  || { ./23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
 
 
 echo "Waiting for all KS and HR to be ready"
-kubectl wait helmrelease -A --all --for=condition=ready --timeout=10m || { 23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
-kubectl wait kustomization -A --all --for=condition=ready --timeout=10m || { 23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
+kubectl wait helmrelease -A --all --for=condition=ready --timeout=10m || { ./23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
+kubectl wait kustomization -A --all --for=condition=ready --timeout=10m || { ./23kectl doctor --kubeconfig hack/ci/secrets/shoot-kubeconfig.yaml; exit 1; }
 
 
 echo -e "23KE Ready ✅"
