c4ddc45e427b71432bcf44ec14cb468d6d1be0fd
Jan Lohage <lohage@23technologies.cloud>
2023-06-06 09:35:39 +0200

    move git upload to shell script
    Signed-off-by: Jan Lohage <lohage@23technologies.cloud>

---
+++ hack/release/upload-git.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+git config --global user.email "trash@23technologies.cloud"
+git config --global user.name "23T Machine User"
+
+tmpDir=$(hack/make-tmp-release-dir.sh GitRepository)
+cp -r $tmpDir/* 23ke-releases
+cd 23ke-releases
+
+if [[ $(git status --porcelain) ]]; then
+  git add .
+  git commit -m "Add release $tag"
+
+  git tag $tag
+  git push
+  git push --tags
+fi
