599af2b13473818fb1fe37cd9be040a760eb5768
Jan Lohage <lohage@23technologies.cloud>
2023-06-06 09:31:35 +0200

    move oci upload to shell script
    Signed-off-by: Jan Lohage <lohage@23technologies.cloud>

---
+++ hack/release/upload-oci.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+source hack/tools/install.sh
+
+install_flux
+
+tmpDir=$(hack/make-tmp-release-dir.sh OCIRepository)
+$FLUX push artifact --creds $creds --source https://23ke.cloud --revision $tag --path="$tmpDir" oci://harbor.23t.dev/23ke-releases/23ke:$tag
