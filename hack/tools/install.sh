f3ce5cd57454fbea6197925f48d92eb344fb3ceb
23t-renovate <renovate@23technologies.cloud>
2023-11-27 08:36:33 +0000

    Update dependency rclone/rclone to v1.65.0

--- hack/tools/install.sh
+++ hack/tools/install.sh
@@ -102,7 +102,7 @@ install_helm() {
 
 install_rclone() {
   # renovate: datasource=github-tags depName=rclone/rclone
-  VERSION=v1.64.2
+  VERSION=v1.65.0
 
   if _isStale $RCLONE $VERSION; then
     ZIP="$RCLONE.zip"
