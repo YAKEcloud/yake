6d204909a34e2e327de48a7ceabd5b513b8559d0
Jens Schneider <schneider@23technologies.cloud>
2023-12-20 13:35:22 +0100

    Add a v to the image tag
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

--- hack/ci/yake-local/build-extension-provider-local.sh
+++ hack/ci/yake-local/build-extension-provider-local.sh
@@ -9,5 +9,5 @@ git clone https://github.com/gardener/gardener gardener-upstream
 cd gardener-upstream || exit 1
 git checkout "$tag"
 
-docker build --build-arg EFFECTIVE_VERSION="$tag"  -t ghcr.io/yakecloud/gardener-extension-provider-local:"$tag"  -f Dockerfile --target gardener-extension-provider-local .
-docker push ghcr.io/yakecloud/gardener-extension-provider-local:"$tag"
+docker build --build-arg EFFECTIVE_VERSION="$tag"  -t ghcr.io/yakecloud/gardener-extension-provider-local:v"$tag"  -f Dockerfile --target gardener-extension-provider-local .
+docker push ghcr.io/yakecloud/gardener-extension-provider-local:v"$tag"
