57bb9ac89d4027bbba1d57763521d952f099ad80
Jens Schneider <schneider@23technologies.cloud>
2023-12-20 14:31:24 +0100

    Update the chart so that no parent directories are included
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

--- hack/ci/yake-local/update-extension-provider-local-image.sh
+++ hack/ci/yake-local/update-extension-provider-local-image.sh
@@ -14,7 +14,7 @@ cd gardener-upstream || exit 1
 git checkout "$newVersion"
 cd ..
 
-export newChart=$(tar -czf - gardener-upstream/charts/gardener/provider-local | base64 -w0 -)
+export newChart=$(tar -C gardener-upstream/charts/gardener/provider-local -czf - . | base64 -w0 -)
 
 yq -i 'select(document_index == 0).providerConfig.values.image=("ghcr.io/yakecloud/gardener-extension-provider-local:" + env(newVersion)) ' hack/ci/yake-local/garden-content/controller-registrations.yaml
 yq -i 'select(document_index == 0).providerConfig.chart=env(newChart) ' hack/ci/yake-local/garden-content/controller-registrations.yaml
