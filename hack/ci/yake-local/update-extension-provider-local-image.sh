f95fc5dd1897e6c110fdcdda46053e9c4be91835
Jens Schneider <schneider@23technologies.cloud>
2023-12-20 14:08:17 +0100

    Also try and update the provider-local chart
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

--- hack/ci/yake-local/update-extension-provider-local-image.sh
+++ hack/ci/yake-local/update-extension-provider-local-image.sh
@@ -8,4 +8,15 @@ fi
 
 source hack/tools/install.sh
 
+git clone https://github.com/gardener/gardener gardener-upstream
+
+cd gardener-upstream || exit 1
+git checkout "$newVersion"
+cd ..
+
+export newChart=$(tar -czf - gardener-upstream/charts/gardener/provider-local | base64 -w0 -)
+
 yq -i 'select(document_index == 0).providerConfig.values.image=("ghcr.io/yakecloud/gardener-extension-provider-local:" + env(newVersion)) ' hack/ci/yake-local/garden-content/controller-registrations.yaml
+yq -i 'select(document_index == 0).providerConfig.chart=env(newChart) ' hack/ci/yake-local/garden-content/controller-registrations.yaml
+
+
