4697abed44b7b9e542643d24249a4b2952687da6
Lothar Bach <bach@23technologies.cloud>
2023-12-20 10:19:51 +0100

    add and run setup_kind_network from gardeners hack/kind-up.sh

--- hack/ci/yake-local/work.sh
+++ hack/ci/yake-local/work.sh
@@ -17,6 +17,38 @@ _create_cluster () {
 	$KUBECTL config set-context --current --namespace=default
 }
 
+# from gardener/gardener hack/kind-up.sh
+# setup_kind_network is similar to kind's network creation logic, ref https://github.com/kubernetes-sigs/kind/blob/23d2ac0e9c41028fa252dd1340411d70d46e2fd4/pkg/cluster/internal/providers/docker/network.go#L50
+# In addition to kind's logic, we ensure stable CIDRs that we can rely on in our local setup manifests and code.
+setup_kind_network() {
+  # check if network already exists
+  local existing_network_id
+  existing_network_id="$(docker network list --filter=name=^kind$ --format='{{.ID}}')"
+
+  if [ -n "$existing_network_id" ] ; then
+    # ensure the network is configured correctly
+    local network network_options network_ipam expected_network_ipam
+    network="$(docker network inspect $existing_network_id | yq '.[]')"
+    network_options="$(echo "$network" | yq '.EnableIPv6 + "," + .Options["com.docker.network.bridge.enable_ip_masquerade"]')"
+    network_ipam="$(echo "$network" | yq '.IPAM.Config' -o=json -I=0)"
+    expected_network_ipam='[{"Subnet":"172.18.0.0/16","Gateway":"172.18.0.1"},{"Subnet":"fd00:10::/64","Gateway":"fd00:10::1"}]'
+
+    if [ "$network_options" = 'true,true' ] && [ "$network_ipam" = "$expected_network_ipam" ] ; then
+      # kind network is already configured correctly, nothing to do
+      return 0
+    else
+      echo "kind network is not configured correctly for local gardener setup, recreating network with correct configuration..."
+      docker network rm $existing_network_id
+    fi
+  fi
+
+  # (re-)create kind network with expected settings
+  docker network create kind --driver=bridge \
+    --subnet 172.18.0.0/16 --gateway 172.18.0.1 \
+    --ipv6 --subnet fd00:10::/64 --gateway fd00:10::1 \
+    --opt com.docker.network.bridge.enable_ip_masquerade=true
+}
+
 _create_loadbalancer () {
   $KUBECTL apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.12/config/manifests/metallb-native.yaml
   $KUBECTL wait --namespace metallb-system --for=condition=ready pod --all --timeout=90s
@@ -246,6 +278,7 @@ install_sipcalc
 install_envsubst
 
 _create_cluster
+setup_kind_network
 _create_loadbalancer
 _create_local_git
 _create_step_ca
