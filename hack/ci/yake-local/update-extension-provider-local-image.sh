d5853ce5aa09d930e2ed8da57ab790080f1a0c34
Jens Schneider <schneider@23technologies.cloud>
2023-12-20 15:39:57 +0100

    Feat/provider local on ghcr (#1239)
    * Prepare automatic updates of the extension-provider-local image
    
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

---
+++ hack/ci/yake-local/update-extension-provider-local-image.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+depName=$1
+newVersion=$2
+if [[ -z "$depName" ]] || [[ $depName != gardener-controlplane ]]; then
+		exit
+fi
+
+source hack/tools/install.sh
+
+install_yq
+
+git clone https://github.com/gardener/gardener gardener-upstream
+
+cd gardener-upstream || exit 1
+git checkout "$newVersion"
+cd ..
+
+export newChart=$(tar -C gardener-upstream/charts/gardener/provider-local -czf - . | base64 -w0 -)
+
+$YQ -i 'select(document_index == 0).providerConfig.values.image=("ghcr.io/yakecloud/gardener-extension-provider-local:" + env(newVersion)) ' hack/ci/yake-local/garden-content/controller-registrations.yaml
+$YQ -i 'select(document_index == 0).providerConfig.chart=env(newChart) ' hack/ci/yake-local/garden-content/controller-registrations.yaml
+
+
