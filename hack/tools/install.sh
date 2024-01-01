fff8e6d88577e0d38fe7edeec3583a89d553e2ac
23t-machine-user <85880907+23t-machine-user@users.noreply.github.com>
2024-01-01 23:10:30 +0100

    Update dependency minio/mc to RELEASE.2023-12-29T20-15-29Z (#1248)
    Co-authored-by: 23t-renovate <renovate@23technologies.cloud>

--- hack/tools/install.sh
+++ hack/tools/install.sh
@@ -82,7 +82,7 @@ install_yq() {
 
 install_mc() {
   # renovate: datasource=github-tags depName=minio/mc versioning=regex:^(?<compatibility>RELEASE\.)(?<major>\d+)-(?<minor>\d+)-(?<patch>\d+)T(?<build>\d+)-\d+-\d+Z$
-  VERSION=RELEASE.2023-12-23T08-47-21Z
+  VERSION=RELEASE.2023-12-29T20-15-29Z
 
   if _isStale $MC $VERSION; then
     curl -L -o "$MC" "https://dl.min.io/client/mc/release/$TOOLS_KERNEL-$TOOLS_ARCH/archive/mc.$VERSION"
