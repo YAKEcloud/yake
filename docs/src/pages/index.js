374a21e0395f76cc2112c100a145aea2a14914fe
Jan Lohage <lohage@23technologies.cloud>
2024-01-23 11:59:01 +0100

    Remove duplicate title
    Signed-off-by: Jan Lohage <lohage@23technologies.cloud>

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -23,11 +23,8 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
