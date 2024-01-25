531b0123e873786c05f78d29f5a9b213a0390a87
Jan Lohage <lohage@23technologies.cloud>
2024-01-25 10:52:16 +0100

    [skip ci] add hack/ci/yake-local script shims for CI
    Signed-off-by: Jan Lohage <lohage@23technologies.cloud>

---
+++ hack/ci/yake-local/build-extension-provider-local.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ../23ke-local || exit 1
+
+./build-extension-provider-local.sh "$@"
---
+++ hack/ci/yake-local/update-extension-provider-local-image.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ../23ke-local || exit 1
+
+./update-extension-provider-local-image.sh "$@"
