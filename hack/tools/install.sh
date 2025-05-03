c88c7af5572332cde4c867bcacc2b61d1deedb7c
23t-machine-user <operations@23technologies.cloud>
2025-05-03 07:15:53 +0000

    Update dependency mikefarah/yq to v4.45.2

--- hack/tools/install.sh
+++ hack/tools/install.sh
@@ -56,7 +56,7 @@ install_kubectl() {
 
 install_yq() {
   # renovate: datasource=github-releases depName=mikefarah/yq
-  VERSION=v4.45.1
+  VERSION=v4.45.2
 
   if _isStale $YQ $VERSION; then
     curl -L -o $YQ "https://github.com/mikefarah/yq/releases/download/$VERSION/yq_${TOOLS_KERNEL}_$TOOLS_ARCH"
